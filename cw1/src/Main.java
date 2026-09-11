1f21da7850c7f6bf3e8da3ad0210aa54add1c21d
Judyta Podesek <test@gmail.com>
2026-09-11 09:17:28 +0200

    instrukcje wypisania

--- cw1/src/Main.java
+++ cw1/src/Main.java
@@ -1,13 +1,17 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+   System.out.println("hello"); //wbisz i zrob enter
+    System.out.println("skrot"); //sout
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
-    }
+    System.out.println("cos bez entera");
+    System.out.println("nowa linia");
+
+    System.out.println("znaki specjalne:");
+    System.out.println("\ttabulacja");
+    System.out.println("\nenter");
+    System.out.println("to jest jak na maszynie\rdo pisania");
+    System.out.println("\"cudyslow\"");
+    System.out.println("\'apostrof\'");
+    System.out.println("backslash: \\");
 }
